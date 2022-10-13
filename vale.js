78a93198be9cfe159213665b6ddd646eab54646f
valeviit <valeviit@icloud.com>
2022-10-13 20:03:47 +0200

    add delete button

---
+++ vale.js
@@ -0,0 +1,24 @@
+//ADD TODO ITEM
+//#content input per scrivere (add iteme onclik del button)
+//if else #category1(business) #category2(personal)
+
+// const addItem = (e) => {
+//     e.preventDefault();
+//     const copy = document.querySelector('.todo-item');
+//     document.querySelector('#todo-list').appendChild(copy);
+
+// }
+
+
+
+
+//Delete button
+function removeItem() {
+    const todoItemDiv = document.querySelector('.todo-item');
+    todoItemDiv.remove();
+}
+
+
+// function editElement() {
+//     console.log();
+// }
