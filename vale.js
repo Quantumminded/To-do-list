fc055c56788ba0597230e78adf768ea954b2340c
valeviit <valeviit@icloud.com>
2022-10-14 15:15:32 +0200

    addTodo  item javascript

--- vale.js
+++ vale.js
@@ -1,14 +1,49 @@
 //ADD TODO ITEM
-//#content input per scrivere (add iteme onclik del button)
-//if else #category1(business) #category2(personal)
+//FORM ID new-todo-form 
+function addTodoList (category,value){
+let newTodo= `<div class="todo-item">
+<label>
+  <input type="checkbox" />
+  <span class='bubble ${category}'></span>
+</label>
 
-// const addItem = (e) => {
-//     e.preventDefault();
-//     const copy = document.querySelector('.todo-item');
-//     document.querySelector('#todo-list').appendChild(copy);
+<div class="todo-content">
+  <input type="text" value="${value}" readonly />
+</div>
 
-// }
+<div class="actions">
+  <button class="edit">Edit</button>
+  <button class="delete" onclick="removeItem()">Delete</button>
+</div>
+</div>` ;
 
+document.querySelector('#todo-list').innerHTML += newTodo;
+}
+
+
+
+
+const form = document.getElementById('new-todo-form');
+const input = document.querySelector('#new-todo-form input');
+const categoryOne = document.querySelector('#category1');
+const categoryTwo = document.querySelector('#category2');
+let category = ''
+categoryOne.addEventListener('click',(e)=>{
+    category =  e.target.value;
+});
+
+categoryTwo.addEventListener('click',(e)=>{
+    category =  e.target.value;
+});
+//console.log(input)
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    if(!input.value){
+        return '';
+    }
+    addTodoList(category,input.value);
+    category= '';
+})
 
 
 
@@ -22,3 +57,4 @@ function removeItem() {
 // function editElement() {
 //     console.log();
 // }
+
