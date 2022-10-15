3201df781edc950baf4a8670be77b1c0b2a55b17
valeviit <valeviit@icloud.com>
2022-10-15 13:59:33 +0200

    create a function for delete todo and edit todo

--- vale.js
+++ vale.js
@@ -1,7 +1,7 @@
-//ADD TODO ITEM
+//ADD TODO ITEM TO THE LIST
 //FORM ID new-todo-form 
-function addTodoList (category,value){
-let newTodo= `<div class="todo-item">
+function addTodoList(category, value) {
+  let newTodo = `<div class="todo-item">
 <label>
   <input type="checkbox" />
   <span class='bubble ${category}'></span>
@@ -13,48 +13,64 @@ let newTodo= `<div class="todo-item">
 
 <div class="actions">
   <button class="edit">Edit</button>
-  <button class="delete" onclick="removeItem()">Delete</button>
+  <button class="delete">Delete</button>
 </div>
 </div>` ;
-
-document.querySelector('#todo-list').innerHTML += newTodo;
+  document.querySelector('#todo-list').innerHTML += newTodo;
+  enableEditDelete();
 }
 
 
 
-
+// create TODO
 const form = document.getElementById('new-todo-form');
 const input = document.querySelector('#new-todo-form input');
 const categoryOne = document.querySelector('#category1');
 const categoryTwo = document.querySelector('#category2');
 let category = ''
-categoryOne.addEventListener('click',(e)=>{
-    category =  e.target.value;
+categoryOne.addEventListener('click', (e) => {
+  category = e.target.value;
 });
 
-categoryTwo.addEventListener('click',(e)=>{
-    category =  e.target.value;
+categoryTwo.addEventListener('click', (e) => {
+  category = e.target.value;
 });
 //console.log(input)
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    if(!input.value){
-        return '';
-    }
-    addTodoList(category,input.value);
-    category= '';
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (!input.value) {
+    return '';
+  }
+  addTodoList(category, input.value);
+  category = '';
 })
 
 
 
-//Delete button
-function removeItem() {
-    const todoItemDiv = document.querySelector('.todo-item');
-    todoItemDiv.remove();
+
+//FUNCTION DELETE AND EDIT (ACTIVATE)
+function enableEditDelete() {
+
+  const editBtn = document.querySelectorAll('.edit');
+  //console.log(editBtn)
+  const deleteBtn = document.querySelectorAll('.delete');
+  editBtn.forEach(element =>
+    element.addEventListener('click', (e) => {
+      let parent = e.target.parentElement.parentElement;
+      let input = parent.querySelector('.todo-content input');
+      input.readOnly = false;
+      input.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+          input.readOnly = true;
+        }
+      })
+    }))
+
+  deleteBtn.forEach(element =>
+    element.addEventListener('click', (e) => {
+      e.target.parentElement.parentElement.remove();
+    }))
+
 }
 
-
-// function editElement() {
-//     console.log();
-// }
 
