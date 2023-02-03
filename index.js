aea2c8beae36122b68f958d2b00903f6774e66b4
AdaN6 <a_disney_da@hotmail.com>
2023-02-03 15:22:27 +0100

    added clear storage function

--- index.js
+++ index.js
@@ -1,119 +1,130 @@
-// window.addEventListener('load', () => {
-//     todos = JSON.parse(localStorage.getItem('todos')) || [];
-//     const nameInput = document.querySelector('#name');
-//     const newTodoForm = document.querySelector('#new-todo-form');
 
-//     const username = localStorage.getItem('username') || '';
-
-//     nameInput.value = username;
-
-//     nameInput.addEventListener('change', e => {
-//         localStorage.setItem('username', e.target.value);
-//     })
-
-
-//     newTodoForm.addEventListener('submit', e => {
-//         e.preventDefault();
-
-//         const todo = {
-//             content: e.target.elements.content.value,
-//             category: e.target.elements.category.value,
-//             done: false,
-//             createdAt: new Date().getTime()
-//         }
-
-//         todos.push(todo);
-
-//         localStorage.setItem('todos', JSON.stringify(todos));
-
-//         e.target.reset();
-
-//         DisplayTodos();
-//     })
-//     DisplayTodos();
-// })
-
-// function DisplayTodos() {
-//     const todoList = document.querySelector('#todo-list');
-
-//     todoList.innerHTML = '';
-
-//     todos.forEach(todo => {
-//         const todoItem = document.createElement('div');
-//         todoItem.classList.add('todo-item');
-
-//         const lable = document.createElement('lable');
-//         const input = document.createElement('input');
-//         const span = document.createElement('span');
-//         const content = document.createElement('div');
-//         const actions = document.createElement('div');
-//         const edit = document.createElement('button');
-//         const deleteButton = document.createElement('button');
-
-//         input.type = 'checkbox';
-//         input.checked = todo.done;
-//         span.classList.add('bubble');
-
-//         if (todo.category == 'personal') {
-//             span.classList.add('personal');
-//         } else {
-//             span.classList.add('business');
-//         }
-
-//         content.classList.add('todo-content');
-//         actions.classList.add('actions');
-//         edit.classList.add('edit');
-//         deleteButton.classList.add('delete');
-
-//         content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
-//         edit.innerHTML = 'Edit';
-//         deleteButton.innerHTML = 'Delete';
-
-//         lable.appendChild(input);
-//         lable.appendChild(span);
-//         actions.appendChild(edit);
-//         actions.appendChild(deleteButton);
-//         todoItem.appendChild(lable);
-//         todoItem.appendChild(content);
-//         todoItem.appendChild(actions);
-
-//         todoList.appendChild(todoItem);
-
-//         if (todo.done) {
-//             todoItem.classList.add("done");
-//         }
-
-//         input.addEventListener("click", e => {
-//             todo.done = e.target.checked;
-//             localStorage.setItem("todos", JSON.stringify(todos));
-
-//             if (todo.done) {
-//                 todoItem.classList.add("done");
-//             } else {
-//                 todoItem.classList.remove("done");
-//             }
-
-//             DisplayTodos();
-//         })
+const form = document.getElementById('new-todo-form');
+const input = document.querySelector('#new-todo-form input');
+const categoryOne = document.querySelector('#category1');
+const categoryTwo = document.querySelector('#category2');
+let category = '';
 
 
 
-//         edit.addEventListener('click', e => {
-//             const input = content.querySelector('input');
-//             input.removeAttribute('readonly', true);
-//             input.focus();
-//             input.addEventListener('blur', e => {
-//                 input.setAttribute('readonly', true);
-//                 todo.content = e.target.value;
-//                 localStorage.setItem('todos', JSON.stringify(todos));
-//                 DisplayTodos();
-//             })
-//         })
+window.addEventListener("load", () => {
+  //todos = JSON.parse(localStorage.getItem("todos")) || [];
+  nameInput = document.querySelector("#name");
+  //const form = document.querySelector("#new-todo-form");
 
-//         deleteButton.addEventListener('click', e => {
-//             todos = todos.filter(t => t != todo);
-//             localStorage.setItem('todos', JSON.stringify(todos));
-//             DisplayTodos();
-//         })
-//     })
-// }
+  const username = localStorage.getItem("username") || "";
+  nameInput.value = username;
+
+  nameInput.addEventListener("change", (e) => {
+    localStorage.setItem("username", e.target.value);
+  });
+
+  loadAll();
+});
+
+
+categoryOne.addEventListener('click', (e) => {
+  category = e.target.value;
+});
+
+categoryTwo.addEventListener('click', (e) => {
+  category = e.target.value;
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (!input.value) {
+    return '';
+  }
+  addTodoList(category, input.value);
+  category = '';
+  const content = document.querySelector("#content");
+
+  content.value = "";
+})
+
+const loadAll = () => {
+  for (const [key, value] of Object.entries(localStorage)) {
+    if (value[0] !== '{') {
+      continue;
+    }
+    let todoParams = JSON.parse(value);
+    let todo = createTodo(todoParams.id, todoParams.category, todoParams.value, todoParams.checked)
+    document.querySelector('#todo-list').innerHTML += todo;
+  }
+}
+
+
+
+//ADD TODO ITEM TO THE LIST
+//FORM ID new-todo-form 
+function addTodoList(category, value) {
+
+  let id = Date.now();
+  let newTodo = createTodo(id, category, value, 'unchecked');
+  localStorage.setItem(id, JSON.stringify({ id: id, category: category, value: value, checked: 'unchecked' }))
+  document.querySelector('#todo-list').innerHTML += newTodo;
+}
+
+function createTodo(id, category, value, checked) {
+
+  return `<div class='todo-item' id='${id}'>
+  <label>
+    <input id="checkbox" type='checkbox' class='checkbox_input' onclick="setChecked(this)" ${checked}/>
+    <span class='check ${category}'></span>
+  </label>
+  <div class='todo-content'>
+    <input type="text" value='${value}' onkeydown="stopEdit(this)" readonly="true""/>
+  </div>
+  <div class='actions'>
+    <button class='edit' onclick="editTodo(this)">Edit</button>
+    <button class='delete' onclick="deleteTodo(this)">Delete</button>
+  </div>
+  </div>`;
+}
+
+function deleteTodo(e) {
+  localStorage.removeItem(e.parentElement.parentElement.id);
+  e.parentElement.parentElement.remove();
+}
+
+const editTodo = (e) => {
+  let input = e.parentElement.parentElement.querySelector('.todo-content input');
+  input.readOnly = false;
+}
+
+const stopEdit = (e) => {
+  if (window.event.key === 'Enter') {
+    e.readOnly = true;
+    let oldTodo = JSON.parse(localStorage.getItem(e.parentElement.parentElement.id));
+    let UpdatedTodo = { ...oldTodo, "value": oldTodo.value = e.value }
+    localStorage.setItem(e.parentElement.parentElement.id, JSON.stringify(UpdatedTodo));
+
+  }
+
+
+}
+
+const setChecked = (e) => {
+  let checked = e.checked ? 'checked' : 'unchecked';
+
+  let todoId = e.parentElement.parentElement.id;
+  let oldTodo = JSON.parse(localStorage.getItem(todoId));
+  let updatedTodo = { ...oldTodo, 'checked': checked };
+  console.log(updatedTodo)
+  localStorage.setItem(todoId, JSON.stringify(updatedTodo));
+
+}
+
+
+// --> Clear Storage button 
+window.onload = function () {
+ document.getElementById("clearButton").onclick = clearStorage;
+
+}
+
+const clearStorage = () => {
+  localStorage.clear()
+  console.log('clear records')
+}
+
